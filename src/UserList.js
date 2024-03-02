40a5b0ab59d808aa24c8d4d47fa799e10b6a3c14
kallymi <mahamatkallymahadjir@gmail.com>
2024-03-02 18:17:27 +0000

    Premier commit

---
+++ src/UserList.js
@@ -0,0 +1,33 @@
+// src/UserList.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const UserList = () => {
+  const [listOfUsers, setListOfUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        setListOfUsers(response.data);
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      <h1>La liste des utilisateurs</h1>
+      <ul className="user-list">
+        {listOfUsers.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
